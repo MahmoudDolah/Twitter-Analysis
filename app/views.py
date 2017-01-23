52d0c0aae7a68bb803e54221c8a0525e67a6714b
Mahmoud <md2989@nyu.edu>
2017-01-22 22:06:16 -0500

    Created form for user to input Twitter handle

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
+from flask import render_template
+from flask import request
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    #user = {'twitter_handle': 'Mike'}
+    user = ""
+    return render_template('index.html', title='Home', user=user)
     
